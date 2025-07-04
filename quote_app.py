ebe6e7c8481a47b74a9d52838aac954d19232900
Pawan Singh <pawan.s03@icloud.com>
2025-07-05 01:01:59 +1200

    Add Python app

---
+++ quote_app.py
@@ -0,0 +1,12 @@
+import random
+
+quotes = [
+    "You're unstoppable!",
+    "Believe in yourself!",
+    "Every day is a new chance to shine.",
+    "Keep pushing forward!",
+    "Success is a journey, not a destination."
+]
+
+print("Motivational Quote of the Day:")
+print(random.choice(quotes))
